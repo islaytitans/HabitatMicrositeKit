6a377e539c753cca7915d6d559ba865f6ba39a52
islaytitans <jonathan.m.robbins@gmail.com>
2016-01-30 11:08:25 +0000

    Dev / Implementation of base methods of Site provider

--- Sitecore.Foundation.Microsite/Providers/DynamicSitesProvider.cs
+++ Sitecore.Foundation.Microsite/Providers/DynamicSitesProvider.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Sitecore.Sites;
 
 namespace Sitecore.Foundation.Microsite.Providers
 {
-    public class DynamicSitesProvider
+    public class DynamicSitesProvider : Sitecore.Sites.SiteProvider
     {
+        public override Site GetSite(string siteName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override SiteCollection GetSites()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
