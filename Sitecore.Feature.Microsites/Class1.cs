80209e0b08a16e09b300b028db64292ac826ca63
islaytitans <jonathan.m.robbins@gmail.com>
2016-01-30 10:04:09 +0000

    Creation

---
+++ Sitecore.Feature.Microsites/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Feature.Microsites
+{
+    public class Class1
+    {
+    }
+}
