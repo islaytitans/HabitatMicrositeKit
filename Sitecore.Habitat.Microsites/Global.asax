ac2afc8a8d228185d94e10249c21f6cc23e87ed8
twentygototen <martin.davies2@gmail.com>
2016-01-30 11:16:54 +0000

    Solution / Added Habitat demo web application

---
+++ Sitecore.Habitat.Microsites/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sitecore.Habitat.Microsites.MvcApplication" Language="C#" %>
---
+++ Sitecore.Habitat.Microsites/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sitecore.Habitat.Microsites.Startup))]
+namespace Sitecore.Habitat.Microsites
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
